66dbd00f8678d23e53fc6e3de09f618712ef1a92
田中 秀明 <tanaka_hideaki@aja-kk.co.jp>
2019-02-06 12:25:41 +0900

    update

--- src/product.game.2048/object/Game2048.js
+++ src/product.game.2048/object/Game2048.js
@@ -1,5 +1,41 @@
 export default class {
 	constructor() {
-		alert();
+		var str, num;
+		// str = '1';
+		// num = this.getCharNumber(this.toZenkaku(str));
+		// console.log(num);
+
+		// str = 'Ｕ';
+		// num = this.getCharNumber(this.toZenkaku(str));
+		// console.log(num);
+
+		// str = 1;
+		// num = this.getCharNumber(this.toZenkaku(str));
+		// console.log(num);
+
+		// str = '🏈';
+		// num = this.getCharNumber(this.toZenkaku(str));
+		// console.log(num);
+
+		str = 'rp9dBxfVuBGidIK7yRIh';
+		num = this.getCharNumber(this.toZenkaku(str));
+		console.log(num);
+	}
+
+	toZenkaku(str) {
+		//16進数の場合
+		return str.toString().replace(/[A-Za-z0-9]/g, function(s) {
+			return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
+		});
+	}
+
+	getCharNumber(zenkaku) {
+		console.log(zenkaku);
+		const n16 = escape(zenkaku).slice(2);
+		return this.change16to10(n16);
+	}
+
+	change16to10(str) {
+		return parseInt(str, 16);
 	}
 }
